424661c788fa3bf4f5b91c3d2011ec94594b1c2d
Masaki Ikegami <salvage0707@gmail.com>
2018-01-23 12:31:49 +0000

    make post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,19 @@ class PostsController < ApplicationController
   	#空のモデルをビューへ渡す
   	@post = Post.new
   end
+
+  def create
+  	#ストロングパラメーターを使用
+  	post = Post.new(post_params)
+  	#dbへ保存する
+  	post.save
+  	#新規投稿画面へリダイレクト
+  	redirect_to '/posts/new'
+  end
+
+  private
+
+  	def post_params
+  		params.require(:post).permit(:title, :body, :category)
+  	end
 end
